4bfd4360011e758971d8c62a7fa1db8128537d04
Sam Mejlumyan <smejlum@gmail.com>
2020-04-28 08:34:57 +0300

    Add ocmock

--- QonversionTests/QDeviceTests.m
+++ QonversionTests/QDeviceTests.m
@@ -1,5 +1,15 @@
 #import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+
 #import "QDevice.h"
+#import "QConstants.h"
+
+// expose private methods for unit testing
+@interface QDevice (Tests)
+
++ (NSString*)getAdvertiserID:(int) maxAttempts;
+
+@end
 
 @interface QDeviceTests : XCTestCase
 @property (nonatomic, strong) QDevice *device;
@@ -8,18 +18,48 @@
 @implementation QDeviceTests
 
 - (void)setUp {
-    self.device = [[QDevice alloc] init];
+    _device = [[QDevice alloc] init];
 }
 
 - (void)tearDown {
-    self.device = nil;
+    _device = nil;
 }
 
-- (void)testThatDevice {
-    XCTAssertNotNil(self.device);
-    XCTAssertEqualObjects(@"ios", self.device.osName);
+- (void)testOSName {
+    XCTAssertEqualObjects(@"ios", _device.osName);
+    XCTAssertNotNil(_device.osVersion);
+}
+
+- (void)testModel {
+    XCTAssertEqualObjects(@"Simulator", _device.model);
+}
+
+- (void)testManufacturer {
+    XCTAssertEqualObjects(@"Apple", _device.manufacturer);
+}
+
+- (void)testAdvertiserID {
+    id mockDeviceInfo = OCMClassMock([QDevice class]);
+    [[mockDeviceInfo expect] getAdvertiserID:5];
+    XCTAssertEqualObjects(nil, _device.advertiserID);
+    [mockDeviceInfo verify];
+    [mockDeviceInfo stopMocking];
+}
+
+- (void)testAppVersion {
+    XCTAssertNotNil(_device);
     
-    XCTAssertNotNil(self.device.appVersion);
-    XCTAssertNotNil(self.device.osVersion);
+    id mockBundle = [OCMockObject niceMockForClass:[NSBundle class]];
+    [[[mockBundle stub] andReturn:mockBundle] mainBundle];
+    
+    NSDictionary *mockDictionary = @{@"CFBundleShortVersionString": keyQVersion};
+    OCMStub([mockBundle infoDictionary]).andReturn(mockDictionary);
+    
+    XCTAssertEqualObjects(keyQVersion, _device.appVersion);
+    [mockBundle stopMocking];
+}
+
+- (void)testVendorID {
+    XCTAssertEqualObjects(_device.vendorID, [[[UIDevice currentDevice] identifierForVendor] UUIDString]);
 }
 @end
---
+++ QonversionTests/QUserInfoTests.m
@@ -0,0 +1,24 @@
+#import <XCTest/XCTest.h>
+
+#import "UserInfo.h"
+#import "QConstants.h
+
+@interface QUserInfoTests : XCTestCase
+
+@end
+
+@implementation QUserInfoTests
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
