89f717a6bbb9fa0086e30ede7042cd87884f6189
Sam Mejlumyan <smejlum@gmail.com>
2020-05-25 19:50:26 +0300

    Add storage protocol

---
+++ Sources/Storage/QLocalStorage.h
@@ -0,0 +1,25 @@
+#import "QBlocksHelpers.h"
+#import <Foundation/Foundation.h>
+
+@protocol QLocalStorage <NSObject>
+@required
+- (void)storeObject:(id)object;
+
+- (void)storeObject:(id)object
+             forKey:(NSString *)key;
+
+- (id)loadObject;
+- (void)loadObjectWithCompletion:(void (^)(id))completion;
+
+- (id)loadObjectForKey:(NSString *)key;
+- (void)loadObjectForKey:(NSString *)key
+          withCompletion:(void(^)(id))completion;
+
+- (void)removeObject;
+- (void)removeObjectForKey:(NSString *)key;
+
+@optional
+- (void)setVersion:(NSString *)version;
+- (NSString *)version;
+
+@end
