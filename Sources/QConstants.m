4c152e45fa6e6512e6837fccf74a7d85726c5e6f
Sam Mejlumyan <smejlum@gmail.com>
2020-05-28 12:45:33 +0300

    Remove extern declaration

--- Sources/QConstants.m
+++ Sources/QConstants.m
@@ -9,7 +9,7 @@ NSString *const keyQUnknownVersion = @"unknown";
 NSString *const keyQInternalUserID = @"keyQInternalUserID";
 NSString *const keyQPropertyReg = @"^(?=.*[a-z])^[-a-z0-9_.:]+$";
 
-extern int const kQPropertiesSendingPeriodInSeconds = 15; // 15 seconds
+int const kQPropertiesSendingPeriodInSeconds = 15; // 15 seconds
 
 NSString *const keyQPropertyFacebookAnonUserID = @"_q_fb_anon_id";
 NSString *const keyQPropertyAppsFlyerUserID = @"_q_appsflyer_user_id";
