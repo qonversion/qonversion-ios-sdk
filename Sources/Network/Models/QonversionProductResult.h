02ea9f4e619adaf2c9d48abeedf07cf3fe71dbba
Sam Mejlumyan <smejlum@gmail.com>
2020-06-19 07:47:46 +0300

    no message

--- Sources/Network/Models/QonversionProductResult.h
+++ Sources/Network/Models/QonversionProductResult.h
@@ -1,6 +1,29 @@
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(unsigned int, QonversionProductType){
+    QonversionProductTypeUnknown = -1,
+    QonversionProductTypeTrial = 0,
+    QonversionProductTypeSubscription = 1,
+    QonversionProductTypeOneTime = 2
+};
+
+typedef NS_ENUM(unsigned int, QonversionProductDuration){
+    QonversionProductDurationUnknown = -1,
+    QonversionProductDurationWeekly = 0,
+    QonversionProductDurationMonthly = 1,
+    QonversionProductDuration3Month = 2,
+    QonversionProductDuration6Month = 3,
+    QonversionProductDurationAnnual = 4,
+    QonversionProductDurationLifetime = 5
+};
+
 @interface QonversionProductResult : NSObject
+
+@property (nonatomic, copy) NSString *qID;
+@property (nonatomic, copy) NSString *storeID;
+
+@property (nonatomic) QonversionProductType type;
+@property (nonatomic) QonversionProductDuration duration;
 
 @end
 
