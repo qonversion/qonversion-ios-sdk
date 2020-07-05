e4516dddcd91f2da9164822258fdb684a671efa6
Sam Mejlumyan <smejlum@gmail.com>
2020-07-05 10:50:17 +0300

    Bump version

--- Sources/QConstants.m
+++ Sources/QConstants.m
@@ -3,7 +3,7 @@
 NSString *const keyQPlatform = @"iOS";
 NSString *const keyQOSName = @"ios";
 
-NSString *const keyQVersion = @"1.3.0";
+NSString *const keyQVersion = @"2.0.0";
 NSString *const keyQUnknownLibrary = @"unknown";
 NSString *const keyQUnknownVersion = @"unknown";
 NSString *const keyQInternalUserID = @"keyQInternalUserID";
