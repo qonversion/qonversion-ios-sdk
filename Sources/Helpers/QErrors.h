b58f9c9bbe06cfffa10e2193f5250317ac973684
Sam Mejlumyan <smejlum@gmail.com>
2020-07-05 14:28:52 +0300

    no message

---
+++ Sources/Helpers/QErrors.h
@@ -0,0 +1,49 @@
+#import <Foundation/Foundation.h>
+
+NS_SWIFT_NAME(Qonversion.Errors)
+@interface QErrors
+
+extern NSErrorDomain const QonversionErrorDomain NS_SWIFT_NAME(Qonversion.ErrorDomain);
+
+typedef NS_ERROR_ENUM(QonversionErrorDomain, QonversionError) {
+    QonversionErrorUnknown = 0,
+    
+    // user cancelled the request, etc.
+    QonversionErrorCancelled,
+    
+    // client is not allowed to issue the request, etc
+    QonversionErrorClientInvalid,
+    
+    // purchase identifier was invalid, etc.
+    QonversionErrorPaymentInvalid,
+    
+    // this device is not allowed to make the payment
+    QonversionErrorPaymentNotAllowed,
+    
+    // product is not available in the current storefront
+    QonversionErrorStoreProductNotAvailable,
+    
+    // user has not allowed access to cloud service information
+    QonversionErrorCloudServicePermissionDenied,
+    
+    // the device could not connect to the nework
+    QonversionErrorCloudServiceNetworkConnectionFailed,
+    
+    // user has revoked permission to use this cloud service
+    QonversionErrorCloudServiceRevoked,
+    
+    // user needs to acknowledge Apple's privacy policy
+    QonversionErrorPrivacyAcknowledgementRequired,
+    
+    // app is attempting to use SKPayment's requestData property, but does not have the appropriate entitlement
+    QonversionErrorUnauthorizedRequestData,
+    
+    // provided shared secret is incorrect, validation unavailable
+    QonversionErrorIncorrectSharedSecret,
+    
+    // failed to connect to Qonversion Backend
+    QonversionErrorConnectionFailed,
+    
+} NS_SWIFT_NAME(Qonversion.Error);
+  
+@end
--- Sources/Helpers/QUtils.h
+++ Sources/Helpers/QUtils.h
@@ -48,9 +48,11 @@ dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
 #endif
 
 #import <Foundation/Foundation.h>
+#import "QErrors.h"
 
 @interface QUtils : NSObject
 
 + (BOOL)isEmptyString:(NSString*)string;
++ (NSError *)errorWithCode:(QonversionError)code;
 
 @end
