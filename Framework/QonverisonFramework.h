87e3aa6cc2ad070f40a26b4bf268560261295a96
Sam Mejlumyan <medjlumyan@gmail.com>
2020-09-20 20:57:07 +0300

    no message

---
+++ Framework/QonverisonFramework.h
@@ -0,0 +1,7 @@
+#import <Qonverison/QNProduct.h>
+#import <Qonverison/QNPermission.h>
+#import <Qonverison/QNLaunchResult.h>
+#import <Qonverison/QNConstants.h>
+#import <Qonverison/QNErrors.h>
+
+#import <Qonverison/Qonversion.h>
