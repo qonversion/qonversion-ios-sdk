b72980741dd621506f6a3a2e5dba143dcac4f727
Sam Mejlumyan <medjlumyan@gmail.com>
2020-09-28 13:37:23 +0300

    Fix warnings

--- Sources/Qonversion/QNProductCenterManager.m
+++ Sources/Qonversion/QNProductCenterManager.m
@@ -353,7 +353,7 @@ static NSString * const kUserDefaultsSuiteName = @"qonversion.product-center.sui
 
 - (void)handleProductsRequestFailed:(NSError *)error {
   NSError *er = [QNErrors errorFromTransactionError:error];
-  QONVERSION_LOG(@"Products requests failed with message: @%", er.description);
+  QONVERSION_LOG(@"Products request failed with message: %@", er.description);
   [self executeProductsBlocks];
 }
 
