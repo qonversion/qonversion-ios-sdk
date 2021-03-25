ed866d6d65ef98cca1ad7e9620cdde3f4e39fcd3
Surik <surik.sarkisyan@gmail.com>
2021-03-26 01:23:08 +0800

    Bump version to 2.12.0

--- Sources/Qonversion/Public/QNConstants.m
+++ Sources/Qonversion/Public/QNConstants.m
@@ -1,6 +1,6 @@
 #import "QNConstants.h"
 
-NSString *const keyQVersion = @"2.11.3";
+NSString *const keyQVersion = @"2.12.0";
 NSString *const keyQUnknownLibrary = @"unknown";
 NSString *const keyQUnknownVersion = @"unknown";
 NSString *const keyQInternalUserID = @"keyQInternalUserID";
