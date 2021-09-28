b6c2436dec1a0c971b9559c339ed5ffa9debae05
Surik <surik.sarkisyan@gmail.com>
2021-09-28 14:07:04 +0300

    Added tests

---
+++ QonversionTests/Managers/QNIdentityManagerTests.m
@@ -0,0 +1,125 @@
+//
+//  QNIdentityManagerTests.m
+//  QonversionTests
+//
+//  Created by Surik Sarkisyan on 17.09.2021.
+//  Copyright © 2021 Qonversion Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+
+#import "XCTestCase+Helpers.h"
+#import "QNIdentityManager.h"
+#import "QNIdentityServiceInterface.h"
+#import "QNUserInfoServiceInterface.h"
+
+@interface QNIdentityManagerTests : XCTestCase
+
+@property (nonatomic, strong) id<QNIdentityServiceInterface> mockIdentityService;
+@property (nonatomic, strong) id<QNUserInfoServiceInterface> mockUserInfoService;
+
+@property (nonatomic, strong) QNIdentityManager *manager;
+
+@end
+
+@implementation QNIdentityManagerTests
+
+- (void)setUp {
+  [super setUp];
+  
+  self.manager = [QNIdentityManager new];
+  
+  self.mockIdentityService = OCMStrictProtocolMock(@protocol(QNIdentityServiceInterface));
+  self.mockUserInfoService = OCMStrictProtocolMock(@protocol(QNUserInfoServiceInterface));
+  
+  self.manager.identityService = self.mockIdentityService;
+  self.manager.userInfoService = self.mockUserInfoService;
+}
+
+- (void)tearDown {
+  self.manager = nil;
+  
+  [super tearDown];
+}
+
+- (void)testSuccessIdentity {
+  // given
+  NSString *userID = @"random_user_id";
+  NSString *anonUserID = @"anon_user_id";
+  NSString *identityID = @"result_identity_id";
+  NSError *randomError = [QNErrors deferredTransactionError]; // just a random error
+  
+  TestBlock testBlock = ^(NSInvocation *invocation) {
+    void(^completioinBlock)(NSString *result, NSError *error);
+    
+    [invocation getArgument:&completioinBlock atIndex:4];
+    
+    completioinBlock(identityID, randomError);
+  };
+  
+  OCMStub([self.mockUserInfoService obtainUserID]).andReturn(anonUserID);
+  OCMStub([self.mockIdentityService identify:userID anonUserID:anonUserID completion:OCMOCK_ANY]).andDo(testBlock);
+  
+  OCMExpect([self.mockIdentityService identify:userID anonUserID:anonUserID completion:OCMOCK_ANY]);
+  OCMExpect([self.mockUserInfoService obtainUserID]);
+  OCMExpect([self.mockUserInfoService storeIdentity:identityID]);
+  
+  // when
+  [self.manager identify:userID completion:^(NSString * _Nullable result, NSError * _Nullable error) {
+    XCTAssertEqual(result, identityID);
+    XCTAssertEqual(randomError, error);
+  }];
+  
+  // then
+  OCMVerify([self.mockUserInfoService obtainUserID]);
+  OCMVerify([self.mockUserInfoService storeIdentity:identityID]);
+  
+  OCMVerify([self.mockIdentityService identify:userID anonUserID:anonUserID completion:OCMOCK_ANY]);
+}
+
+- (void)testFailureIdentity {
+  // given
+  NSString *userID = @"random_user_id";
+  NSString *anonUserID = @"anon_user_id";
+  
+  NSError *randomError = [QNErrors deferredTransactionError]; // just a random error
+  
+  TestBlock testBlock = ^(NSInvocation *invocation) {
+    void(^completioinBlock)(NSString *result, NSError *error);
+    
+    [invocation getArgument:&completioinBlock atIndex:4];
+    
+    completioinBlock(nil, randomError);
+  };
+  
+  OCMStub([self.mockUserInfoService obtainUserID]).andReturn(anonUserID);
+  OCMStub([self.mockIdentityService identify:userID anonUserID:anonUserID completion:OCMOCK_ANY]).andDo(testBlock);
+  
+  OCMExpect([self.mockIdentityService identify:userID anonUserID:anonUserID completion:OCMOCK_ANY]);
+  OCMExpect([self.mockUserInfoService obtainUserID]);
+  
+  // when
+  [self.manager identify:userID completion:^(NSString * _Nullable result, NSError * _Nullable error) {
+    XCTAssertEqual(error, randomError);
+    XCTAssertNil(result);
+  }];
+  
+  // then
+  OCMVerify([self.mockUserInfoService obtainUserID]);
+  
+  OCMVerify([self.mockIdentityService identify:userID anonUserID:anonUserID completion:OCMOCK_ANY]);
+}
+
+- (void)testLogoutIfNeeded {
+  // given
+  OCMExpect([self.mockUserInfoService logoutIfNeeded]);
+  
+  // when
+  [self.manager logoutIfNeeded];
+  
+  // then
+  OCMVerifyAll(self.mockUserInfoService);
+}
+
+@end
---
+++ QonversionTests/Services/QNIdentityServiceTests.m
@@ -0,0 +1,98 @@
+//
+//  QNIdentityServiceTests.m
+//  QonversionTests
+//
+//  Created by Surik Sarkisyan on 16.09.2021.
+//  Copyright © 2021 Qonversion Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+#import "XCTestCase+Unmock.h"
+#import "XCTestCase+Helpers.h"
+
+#import "QNIdentityService.h"
+#import "QNAPIClient.h"
+
+@interface QNIdentityServiceTests : XCTestCase
+
+@property (nonatomic, strong) QNIdentityService *service;
+@property (nonatomic, strong) QNAPIClient *mockApiClient;
+
+@end
+
+@implementation QNIdentityServiceTests
+
+- (void)setUp {
+  [super setUp];
+  
+  self.mockApiClient = OCMClassMock([QNAPIClient class]);
+  
+  self.service = [QNIdentityService new];
+  
+  self.service.apiClient = self.mockApiClient;
+}
+
+- (void)tearDown {
+  [self unmock:self.mockApiClient];
+  
+  self.service = nil;
+  
+  [super tearDown];
+}
+
+- (void)testSuccessIdentity {
+  // given
+  NSString *userID = @"random_user_id";
+  NSString *anonUserID = @"anon_user_id";
+  NSString *identityID = @"identity_id";
+  
+  NSDictionary *data = @{@"data": @{@"anon_id": identityID}};
+  
+  TestBlock testBlock = ^(NSInvocation *invocation) {
+    void(^completioinBlock)(NSDictionary *data, NSError *error);
+    
+    [invocation getArgument:&completioinBlock atIndex:4];
+    
+    completioinBlock(data, nil);
+  };
+  
+  OCMStub([self.mockApiClient createIdentityForUserID:userID anonUserID:anonUserID completion:OCMOCK_ANY]).andDo(testBlock);
+  
+  // when
+  [self.service identify:userID anonUserID:anonUserID completion:^(NSString * _Nullable result, NSError * _Nullable error) {
+    // then
+    XCTAssertEqual(result, identityID);
+    XCTAssertNil(error);
+  }];
+}
+
+- (void)testFailureIdentity {
+  // given
+  NSString *userID = @"random_user_id";
+  NSString *anonUserID = @"anon_user_id";
+  NSString *identityID = @"identity_id";
+  NSError *randomError = [QNErrors deferredTransactionError]; // just a random error
+  
+  NSDictionary *data = @{@"data": @{@"anon_id_wrong_key": identityID}};
+  
+  TestBlock testBlock = ^(NSInvocation *invocation) {
+    void(^completioinBlock)(NSDictionary *data, NSError *error);
+    
+    [invocation getArgument:&completioinBlock atIndex:4];
+    
+    completioinBlock(data, randomError);
+  };
+  
+  OCMStub([self.mockApiClient createIdentityForUserID:userID anonUserID:anonUserID completion:OCMOCK_ANY]).andDo(testBlock);
+  
+  // when
+  [self.service identify:userID anonUserID:anonUserID completion:^(NSString * _Nullable result, NSError * _Nullable error) {
+    // then
+    XCTAssertEqual(error, randomError);
+    XCTAssertNil(result);
+  }];
+}
+
+
+@end
