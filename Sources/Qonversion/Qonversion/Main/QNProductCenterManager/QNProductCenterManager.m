d024f2ba81fa6fb1a7f6be8b824cbaf8f67b4ccc
Surik <surik.sarkisyan@gmail.com>
2022-06-02 15:34:27 +0300

    Updated finish transaction logic

--- Sources/Qonversion/Qonversion/Main/QNProductCenterManager/QNProductCenterManager.m
+++ Sources/Qonversion/Qonversion/Main/QNProductCenterManager/QNProductCenterManager.m
@@ -890,8 +890,6 @@ static NSString * const kUserDefaultsSuiteName = @"qonversion.product-center.sui
   self.purchaseModels[product.productIdentifier] = nil;
   [self.storeKitService receipt:^(NSString * receipt) {
     [weakSelf.apiClient purchaseRequestWith:product transaction:transaction receipt:receipt purchaseModel:purchaseModel completion:^(NSDictionary * _Nullable dict, NSError * _Nullable error) {
-      [weakSelf.storeKitService finishTransaction:transaction];
-      
       QNPurchaseCompletionHandler _purchasingBlock = weakSelf.purchasingBlocks[product.productIdentifier];
       @synchronized (weakSelf) {
         [weakSelf.purchasingBlocks removeObjectForKey:product.productIdentifier];
@@ -910,6 +908,8 @@ static NSString * const kUserDefaultsSuiteName = @"qonversion.product-center.sui
         run_block_on_main(_purchasingBlock, @{}, result.error, NO);
         return;
       }
+      
+      [weakSelf.storeKitService finishTransaction:transaction];
       
       QNUser *user = [QNMapper fillUser:result.data];
       weakSelf.user = user;
