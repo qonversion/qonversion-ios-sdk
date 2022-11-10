2d8027936b315d05db2caf0d01de1aefcc9c67a8
Surik <surik.sarkisyan@gmail.com>
2022-11-10 18:02:39 +0400

    Removed unused file

--- Framework/QonversionFramework.h
+++ Framework/QonversionFramework.h
@@ -3,7 +3,6 @@
 #import <Qonversion/QNLaunchResult.h>
 #import <Qonversion/QNPromoPurchasesDelegate.h>
 #import <Qonversion/QONEntitlementsUpdateListener.h>
-#import <Qonversion/QNPurchasesDelegate.h>
 #import <Qonversion/QNEntitlement.h>
 #import <Qonversion/QNProduct.h>
 #import <Qonversion/QNStoreKitSugare.h>
