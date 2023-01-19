ec88ea0235ed57020dec66e83e522cdfbd94d423
Surik <surik.sarkisyan@gmail.com>
2023-01-19 16:27:30 +0400

    Updated imports

--- Framework/QonversionFramework.h
+++ Framework/QonversionFramework.h
@@ -16,6 +16,7 @@
 #import <Qonversion/QONAutomations.h>
 #import <Qonversion/QONAutomationsDelegate.h>
 #import <Qonversion/QONScreenCustomizationDelegate.h>
+#import <Qonversion/QONScreenPresentationConfiguration.h>
 #import <Qonversion/QONActionResult.h>
 
 #import <Qonversion/QONUser.h>
