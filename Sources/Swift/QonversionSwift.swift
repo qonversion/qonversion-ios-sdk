2e7343679d6f542281f0132a533c8dd9647e08e9
Surik <action@github.com>
2023-07-18 16:41:16 +0400

    Updated StoreKit2 logic

--- Sources/Swift/QonversionSwift.swift
+++ Sources/Swift/QonversionSwift.swift
@@ -11,7 +11,7 @@ import Foundation
 public class QonversionSwift {
   static public let shared = QonversionSwift()
   
-  private var storeKitService: StoreKit2Service?
+  private var storeKitService: StoreKit2ServiceInterface?
   
   init() {
     if #available(iOS 15.0, *) {
@@ -21,7 +21,33 @@ public class QonversionSwift {
   
   /// Contact us before you start using this function.
   /// Call this function to sync purchases if you are using StoreKit2.
+  @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
   public func syncStoreKit2Purchases() {
-    storeKitService?.syncTransactions()
+    Task.init {
+        try await storeKitService?.syncTransactions()
+    }
+  }
+  
+  /// Contact us before you start using this function.
+  /// Call this function to sync purchases if you are using StoreKit2.
+  @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+  public func syncStoreKit2Transactions() async {
+    try? await storeKitService?.syncTransactions()
+  }
+  
+  /// Call this function to sync StoreKit2 transaction with Qonversion.
+  /// - Parameters:
+  ///   - transaction: StoreKit2 transaction
+  @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+  public func handleTransaction(_ transaction: Transaction) async {
+    try? await storeKitService?.handleTransaction(transaction)
+  }
+  
+  /// Call this function to sync StoreKit2 transactions with Qonversion.
+  /// - Parameters:
+  ///   - transactions: an array of StoreKit2 transactions
+  @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+  public func handleTransactions(_ transactions: [Transaction]) async {
+    try? await storeKitService?.handleTransactions(transactions)
   }
 }
