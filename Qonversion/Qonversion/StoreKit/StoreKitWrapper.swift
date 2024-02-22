4ba520ada697b24aec927beef63d878329ce3515
Surik <action@github.com>
2024-02-22 19:13:26 +0400

    Added storekit 2 wrapper logic

--- Qonversion/Qonversion/StoreKit/StoreKitWrapper.swift
+++ Qonversion/Qonversion/StoreKit/StoreKitWrapper.swift
@@ -11,7 +11,11 @@ import StoreKit
 @available(iOS 15.0, *)
 final class StoreKitWrapper: StoreKitWrapperInterface {
     
-    var allTransactions: [Transaction] = []
+    let delegate: StoreKitWrapperDelegate
+    
+    init(delegate: StoreKitWrapperDelegate) {
+        self.delegate = delegate
+    }
     
     func products(for ids:[String]) async throws -> [Product] {
         let products: [Product] = try await Product.products(for: ids)
@@ -19,46 +23,78 @@ final class StoreKitWrapper: StoreKitWrapperInterface {
         return products
     }
     
-    func currentEntitlements() -> Transaction.Transactions {
-        return Transaction.currentEntitlements
+    func currentEntitlements() async -> [Transaction] {
+        return await fetchTransactions(for: Transaction.currentEntitlements)
     }
     
     func restore() async throws {
         return try await AppStore.sync()
     }
     
-    func allTransactions() async -> Transaction.Transactions? {
-        Task {
-            for await transaction in Transaction.all {
-                switch transaction {
-                case .verified(let verifiedTransaction):
-                    allTransactions.append(verifiedTransaction)
-                default:
-                    break
-                }
-            }
-            
-            return allTransactions
-        }
+    func fetchAll() async -> [Transaction] {
+        return await fetchTransactions(for: Transaction.all)
+    }
+    
+    func fetchUnfinished() async -> [Transaction] {
+        return await fetchTransactions(for: Transaction.unfinished)
     }
     
     func finish(transaction: Transaction) async {
         await transaction.finish()
     }
     
-    func subscribe() async {
-        for await update in StoreKit.Transaction.updates {
-            if let transaction = try? update.payloadValue {
-                
+    func purchase(product: Product) async throws -> Transaction {
+        let result: Product.PurchaseResult = try await product.purchase()
+        
+        switch result {
+        case .success(let verificationResult):
+            switch verificationResult {
+            case .verified(let transaction):
+                return transaction
+            case .unverified(let transaction, let verificationError):
+                // throw error here using verification error
+                throw QonversionError(type: .critical)
             }
+        case .userCancelled:
+            // update error here
+            throw QonversionError(type: .critical)
+        @unknown default:
+            // update error here
+            throw QonversionError(type: .critical)
         }
     }
     
+    func subscribe() async -> [Transaction] {
+        return await fetchTransactions(for: Transaction.updates)
+    }
+    
+    @available(iOS 16.4, *)
+    func subscribeToPromoPurchases() {
+        Task.detached {
+            for await purchaseIntent in PurchaseIntent.intents {
+                self.delegate.promoPurchaseIntent(product: purchaseIntent.product)
+            }
+        }
+    }
     
         
     @available(iOS 16.0, *)
     func presentOfferCodeRedeemSheet(in scene: UIWindowScene) async throws {
         try await AppStore.presentOfferCodeRedeemSheet(in: scene)
+    }
+    
+    private func fetchTransactions(for type: Transaction.Transactions) async -> [Transaction] {
+      var transasctions: [Transaction] = []
+      for await transaction in type {
+        switch transaction {
+        case .verified(let verifiedTransaction):
+          transasctions.append(verifiedTransaction)
+        default:
+          break
+        }
+      }
+      
+      return transasctions
     }
     
 }
--- Qonversion/Qonversion/StoreKit/StoreKitWrapperInterface.swift
+++ Qonversion/Qonversion/StoreKit/StoreKitWrapperInterface.swift
@@ -6,7 +6,26 @@
 //
 
 import Foundation
+import StoreKit
 
+@available(iOS 15.0, *)
 protocol StoreKitWrapperInterface {
+    
+    func products(for ids:[String]) async throws -> [Product]
+    
+    func currentEntitlements() async -> [Transaction]
+    
+    func restore() async throws
+    
+    func fetchAll() async -> [Transaction]
+    
+    func fetchUnfinished() async -> [Transaction]
+    
+    func finish(transaction: Transaction) async
+    
+    func subscribe() async -> [Transaction]
+        
+    @available(iOS 16.0, *)
+    func presentOfferCodeRedeemSheet(in scene: UIWindowScene) async throws
     
 }
