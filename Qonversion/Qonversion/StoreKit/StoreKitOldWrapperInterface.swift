63b8e761c9f4284633ae8ba076990bc5040a80e2
Surik <action@github.com>
2024-02-28 11:54:12 +0400

    Removed useless code and updated few functions naming

--- Qonversion/Qonversion/StoreKit/StoreKitOldWrapperInterface.swift
+++ Qonversion/Qonversion/StoreKit/StoreKitOldWrapperInterface.swift
@@ -11,6 +11,8 @@ protocol StoreKitOldWrapperInterface {
     
     func loadProducts(for ids:[String])
     func restore()
+    
+    @available(iOS 14.0, *)
     func presentCodeRedemptionSheet()
     func purchase(product: SKProduct)
     func finish(transaction: SKPaymentTransaction)
