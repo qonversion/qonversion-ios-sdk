80e141749e271235c325707c3f97bd90f067a367
Kamo Spertsyan <spertsiankamo@gmail.com>
2024-03-01 11:28:35 +0300

    Sending properties.

--- Qonversion/Qonversion/UserProperties/UserProperty.swift
+++ Qonversion/Qonversion/UserProperties/UserProperty.swift
@@ -7,26 +7,9 @@
 
 import Foundation
 
-struct UserProperty : Decodable, Encodable, Equatable {
-    
+struct UserProperty : Decodable, Encodable, Equatable, Hashable {
+
     let key: String
     let value: String
-    let definedKey: UserPropertyKey
-
-    private enum CodingKeys: String, CodingKey {
-        case key, value
-    }
-
-    init(key: String, value: String) {
-        self.key = key
-        self.value = value
-        definedKey = UserPropertyKey(rawValue: key) ?? .custom
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        let key = try container.decode(String.self, forKey: .key)
-        let value = try container.decode(String.self, forKey: .value)
-        self.init(key: key, value: value)
-    }
+    var definedKey: UserPropertyKey { UserPropertyKey(rawValue: key) ?? .custom }
 }
