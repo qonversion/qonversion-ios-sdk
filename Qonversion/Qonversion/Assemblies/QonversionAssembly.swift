81076b0e4a826b5c9dafc1496d2a36023b6e0d5d
Surik <action@github.com>
2024-03-27 20:38:13 +0400

    Review fixes

--- Qonversion/Qonversion/Assemblies/QonversionAssembly.swift
+++ Qonversion/Qonversion/Assemblies/QonversionAssembly.swift
@@ -10,21 +10,20 @@ import Foundation
 final class QonversionAssembly {
     
     private let servicesAssembly: ServicesAssembly
+    private let miscAssembly: MiscAssembly
     
     required init(apiKey: String, userDefaults: UserDefaults?) {
         self.servicesAssembly = ServicesAssembly(apiKey: apiKey, userDefaults: userDefaults)
+        self.miscAssembly = MiscAssembly()
     }
     
     func userPropertiesManager() -> UserPropertiesManagerInterface {
-        let requestProcessor = servicesAssembly.requestProcessor()
-        let propertiesStorage = UserPropertiesStorage()
-        let userPropertiesManager = UserPropertiesManager(requestProcessor: requestProcessor, propertiesStorage: propertiesStorage, delayCalculator: delayCalculator(), internalConfig: InternalConfig(userId: "da"))
+        let requestProcessor: RequestProcessorInterface = servicesAssembly.requestProcessor()
+        let delayCalculator: IncrementalDelayCalculator = miscAssembly.delayCalculator()
+        let propertiesStorage: PropertiesStorage = miscAssembly.userPropertiesStorage()
+        let userPropertiesManager = UserPropertiesManager(requestProcessor: requestProcessor, propertiesStorage: propertiesStorage, delayCalculator: delayCalculator, internalConfig: InternalConfig(userId: "da"))
         
         return userPropertiesManager
-    }
-    
-    func delayCalculator() -> IncrementalDelayCalculator {
-        return IncrementalDelayCalculator()
     }
     
 }
