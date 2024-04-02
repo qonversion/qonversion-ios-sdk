f915d6caaa3ad45111faedca130581e1070451de
SpertsyanKM <SpertsyanKM@users.noreply.github.com>
2024-04-02 15:15:06 +0000

    [create-pull-request] automated change

--- Sources/Qonversion/Public/QONConfiguration.m
+++ Sources/Qonversion/Public/QONConfiguration.m
@@ -9,7 +9,7 @@
 #import "QONConfiguration.h"
 #import "QNAPIConstants.h"
 
-static NSString *const kSDKVersion = @"5.8.2";
+static NSString *const kSDKVersion = @"5.8.3";
 
 @interface QONConfiguration ()
 
