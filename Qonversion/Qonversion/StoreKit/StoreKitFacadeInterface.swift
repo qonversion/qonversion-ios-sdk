26ea502044294e332324e03c04e875eb885305b9
Surik <action@github.com>
2024-04-09 12:13:00 +0400

    Updated internal classes usage

--- Qonversion/Qonversion/StoreKit/StoreKitFacadeInterface.swift
+++ Qonversion/Qonversion/StoreKit/StoreKitFacadeInterface.swift
@@ -11,16 +11,16 @@ protocol StoreKitFacadeInterface {
     #warning("replace all return types")
     func products(for ids:[String]) async throws -> [String]
     
-    func currentEntitlements() async -> [Qonversion.Transaction]
+    func currentEntitlements() async -> [Transaction]
     
-    func restore() async throws -> [Qonversion.Transaction]
+    func restore() async throws -> [Transaction]
     
     func finish(transaction: SKPaymentTransaction)
     
     @available(iOS 15.0, *)
     func finish(transaction: StoreKit.Transaction) async
     
-    func subscribe() async -> [Qonversion.Transaction]
+    func subscribe() async -> [Transaction]
         
     @available(iOS 16.0, *)
     func presentOfferCodeRedeemSheet(in scene: UIWindowScene) async throws
