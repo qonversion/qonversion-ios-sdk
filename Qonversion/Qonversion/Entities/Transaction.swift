100846c147b4419c196c64190095fca9d8edd51a
Surik <action@github.com>
2024-04-09 14:52:18 +0400

    Updated types

--- Qonversion/Qonversion/Entities/Transaction.swift
+++ Qonversion/Qonversion/Entities/Transaction.swift
@@ -64,7 +64,7 @@ extension Qonversion {
                 case code
                 
                 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
-                static func from(transaction: StoreKit.Transaction?) -> Transaction.Offer.OfferType? {
+                static func from(transaction: StoreKit.Transaction?) -> Qonversion.Transaction.Offer.OfferType? {
                     guard let transaction: StoreKit.Transaction = transaction else { return nil }
                    
                     let type: StoreKit.Transaction.OfferType?
@@ -78,11 +78,11 @@ extension Qonversion {
                     
                     switch type {
                     case .introductory:
-                        return Transaction.Offer.OfferType.introductory
+                        return Qonversion.Transaction.Offer.OfferType.introductory
                     case .promotional:
-                        return Transaction.Offer.OfferType.promotional
+                        return Qonversion.Transaction.Offer.OfferType.promotional
                     case .code:
-                        return Transaction.Offer.OfferType.code
+                        return Qonversion.Transaction.Offer.OfferType.code
                     default:
                         return nil
                     }
@@ -95,15 +95,15 @@ extension Qonversion {
                 case payUpFront
                 
                 @available(iOS 17.2, macOS 14.2, tvOS 17.2, watchOS 10.2, visionOS 1.1, *)
-                static func from(paymentMode: StoreKit.Transaction.Offer.PaymentMode?) -> Transaction.Offer.PaymentMode? {
+                static func from(paymentMode: StoreKit.Transaction.Offer.PaymentMode?) -> Qonversion.Transaction.Offer.PaymentMode? {
                     guard let paymentMode = paymentMode else { return nil }
                     switch paymentMode {
                     case .freeTrial:
-                        return Transaction.Offer.PaymentMode.freeTrial
+                        return Qonversion.Transaction.Offer.PaymentMode.freeTrial
                     case .payAsYouGo:
-                        return Transaction.Offer.PaymentMode.payAsYouGo
+                        return Qonversion.Transaction.Offer.PaymentMode.payAsYouGo
                     case .payUpFront:
-                        return Transaction.Offer.PaymentMode.payUpFront
+                        return Qonversion.Transaction.Offer.PaymentMode.payUpFront
                     default:
                         return nil
                     }
@@ -114,9 +114,9 @@ extension Qonversion {
             let type: Transaction.Offer.OfferType?
             
             @available(iOS 17.2, macOS 14.2, tvOS 17.2, watchOS 10.2, visionOS 1.1, *)
-            var paymentMode: Transaction.Offer.PaymentMode? {
+            var paymentMode: Qonversion.Transaction.Offer.PaymentMode? {
                 guard let offer = _offer as? StoreKit.Transaction.Offer else { return nil }
-                return Transaction.Offer.PaymentMode.from(paymentMode: offer.paymentMode)
+                return Qonversion.Transaction.Offer.PaymentMode.from(paymentMode: offer.paymentMode)
             }
             
             @available(iOS 17.2, macOS 14.2, tvOS 17.2, watchOS 10.2, visionOS 1.1, *)
@@ -127,7 +127,7 @@ extension Qonversion {
             
             @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
             init?(with transaction: StoreKit.Transaction) {
-                self.type = Transaction.Offer.OfferType.from(transaction: transaction)
+                self.type = Qonversion.Transaction.Offer.OfferType.from(transaction: transaction)
                 
                 if #available(iOS 17.2, macOS 14.2, tvOS 17.2, watchOS 10.2, visionOS 1.1, *) {
                     guard let offer = transaction.offer else { return nil }
@@ -176,10 +176,10 @@ extension Qonversion {
         public var isUpgraded: Bool? { storeKitTransaction?.isUpgraded }
 
         // Workaround to make offer variable available for specific OS versions
-        private let _offer: Transaction.Offer?
+        private let _offer: Qonversion.Transaction.Offer?
         
         @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
-        public var offer: Transaction.Offer? { _offer }
+        public var offer: Qonversion.Transaction.Offer? { _offer }
         
         @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
         public var revocationDate: Date? { storeKitTransaction?.revocationDate }
@@ -188,22 +188,22 @@ extension Qonversion {
         public var appAccountToken: UUID? { storeKitTransaction?.appAccountToken }
         
         @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, visionOS 1.0, *)
-        public var environment: Transaction.Environment? { Transaction.Environment(rawValue: storeKitTransaction?.environment.rawValue ?? "") }
+        public var environment: Qonversion.Transaction.Environment? { Qonversion.Transaction.Environment(rawValue: storeKitTransaction?.environment.rawValue ?? "") }
         
         @available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *)
         public var reason: Transaction.Reason {
             guard let storeKitTransaction = storeKitTransaction,
-                  let reason = Transaction.Reason(rawValue: storeKitTransaction.reason.rawValue)
-            else { return Transaction.Reason.purchase }
+                  let reason = Qonversion.Transaction.Reason(rawValue: storeKitTransaction.reason.rawValue)
+            else { return Qonversion.Transaction.Reason.purchase }
             
             return reason
         }
         
         public let price: Decimal?
         
-        public let currency: Transaction.Currency?
+        public let currency: Qonversion.Transaction.Currency?
         
-        public let storefront: Storefront?
+        public let storefront: Qonversion.Storefront?
         
         @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
         public var deviceVerification: Data? { storeKitTransaction?.deviceVerification }
@@ -215,10 +215,10 @@ extension Qonversion {
         public var signedDate: Date? { storeKitTransaction?.signedDate }
         
         @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
-        public var ownershipType: Transaction.OwnershipType {
+        public var ownershipType: Qonversion.Transaction.OwnershipType {
             guard let storeKitTransaction = storeKitTransaction,
-                  let ownershipType = Transaction.OwnershipType(rawValue: storeKitTransaction.ownershipType.rawValue)
-            else { return Transaction.OwnershipType.purchased }
+                  let ownershipType = Qonversion.Transaction.OwnershipType(rawValue: storeKitTransaction.ownershipType.rawValue)
+            else { return Qonversion.Transaction.OwnershipType.purchased }
             
             return ownershipType
         }
@@ -239,7 +239,7 @@ extension Qonversion {
             self.purchaseDate = transaction.transactionDate
             self.originalPurchaseDate = transaction.original?.transactionDate
             self.purchasedQuantity = transaction.payment.quantity
-            self.storefront = Storefront(countryCode: SKPaymentQueue.default().storefront?.countryCode, id: SKPaymentQueue.default().storefront?.identifier)
+            self.storefront = Qonversion.Storefront(countryCode: SKPaymentQueue.default().storefront?.countryCode, id: SKPaymentQueue.default().storefront?.identifier)
             self.price = product.price as Decimal
             
             let currencyCode: String?
@@ -249,7 +249,7 @@ extension Qonversion {
                 currencyCode = product.priceLocale.currencyCode
             }
             
-            self.currency = Transaction.Currency(identifier: currencyCode, symbol: product.priceLocale.currencySymbol)
+            self.currency = Qonversion.Transaction.Currency(identifier: currencyCode, symbol: product.priceLocale.currencySymbol)
             
             self._offer = nil
             self._storeKitTransaction = nil
@@ -269,19 +269,19 @@ extension Qonversion {
             self.price = transaction.price
             
             if #available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, visionOS 1.0, *) {
-                self.currency = Transaction.Currency(identifier: transaction.currency?.identifier, symbol: transaction.currency?.currencySymbol())
+                self.currency = Qonversion.Transaction.Currency(identifier: transaction.currency?.identifier, symbol: transaction.currency?.currencySymbol())
             } else {
-                self.currency = Transaction.Currency(identifier: transaction.currencyCode, symbol: transaction.currencyCode?.toCurrencySymbol())
+                self.currency = Qonversion.Transaction.Currency(identifier: transaction.currencyCode, symbol: transaction.currencyCode?.toCurrencySymbol())
             }
             
             if #available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, visionOS 1.0, *) {
-                self.storefront = Storefront(countryCode: transaction.storefront.countryCode, id: transaction.storefront.id)
+                self.storefront = Qonversion.Storefront(countryCode: transaction.storefront.countryCode, id: transaction.storefront.id)
             } else {
-                self.storefront = Storefront(countryCode: transaction.storefrontCountryCode, id: nil)
+                self.storefront = Qonversion.Storefront(countryCode: transaction.storefrontCountryCode, id: nil)
             }
             
             if #available(iOS 17.2, macOS 14.2, tvOS 17.2, watchOS 10.2, visionOS 1.1, *) {
-                self._offer = Transaction.Offer(with: transaction)
+                self._offer = Qonversion.Transaction.Offer(with: transaction)
             } else {
                 self._offer = nil
             }
