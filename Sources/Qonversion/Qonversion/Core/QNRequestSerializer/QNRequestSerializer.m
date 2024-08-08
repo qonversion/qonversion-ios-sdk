59626970ea17f9c531aaf2de5fdd8227056e7f07
Surik <action@github.com>
2024-08-08 17:59:19 +0400

    Fixed warning

--- Sources/Qonversion/Qonversion/Core/QNRequestSerializer/QNRequestSerializer.m
+++ Sources/Qonversion/Qonversion/Core/QNRequestSerializer/QNRequestSerializer.m
@@ -94,7 +94,7 @@ NS_ASSUME_NONNULL_BEGIN
   NSMutableDictionary *result = [NSMutableDictionary new];
   
   result[@"productIdentifier"] = product.storeID;
-  if (@available(iOS 12.2, macOS 10.14.4, watchOS 6.2, visionOS 1.0, *)) {
+  if (@available(iOS 12.2, macOS 10.14.4, watchOS 6.2, tvOS 12.2, visionOS 1.0, *)) {
     result[@"discountIdentifier"] = productDiscount.identifier;
   }
   result[@"idetntityId"] = identityId;
