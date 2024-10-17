f0c0825eb0a83218bab47a44eacf2c35c6d81a10
Kamo Spertsyan <spertsiankamo@gmail.com>
2024-10-17 12:33:48 +0300

    Private members access fixes

--- Sources/Qonversion/Qonversion/Main/QNUserPropertiesManager/QNUserPropertiesManager.m
+++ Sources/Qonversion/Qonversion/Main/QNUserPropertiesManager/QNUserPropertiesManager.m
@@ -103,10 +103,10 @@ static NSString * const kBackgroundQueueName = @"qonversion.background.queue.nam
 
 - (void)sendPropertiesWithDelay:(NSUInteger)delay {
   @synchronized (self) {
-    if (!_sendingScheduled) {
-      _sendingScheduled = YES;
+    if (!self.sendingScheduled) {
+      self.sendingScheduled = YES;
       __block __weak QNUserPropertiesManager *weakSelf = self;
-      [_backgroundQueue addOperationWithBlock:^{
+      [self.backgroundQueue addOperationWithBlock:^{
         dispatch_async(dispatch_get_main_queue(), ^{
           [weakSelf performSelector:@selector(sendPropertiesInBackground) withObject:nil afterDelay:delay];
         });
@@ -117,7 +117,7 @@ static NSString * const kBackgroundQueueName = @"qonversion.background.queue.nam
 
 - (void)sendPropertiesInBackground {
   @synchronized (self) {
-    _sendingScheduled = NO;
+    self.sendingScheduled = NO;
   }
   [self sendProperties];
 }
@@ -127,31 +127,31 @@ static NSString * const kBackgroundQueueName = @"qonversion.background.queue.nam
 }
 
 - (void)sendProperties:(BOOL)force {
-  if ([QNUtils isEmptyString:_apiClient.apiKey]) {
+  if ([QNUtils isEmptyString:self.apiClient.apiKey]) {
     QONVERSION_ERROR(@"ERROR: apiKey cannot be nil or empty, set apiKey with launchWithKey:");
     return;
   }
   
   @synchronized (self) {
-    if (_updatingCurrently && !force) {
+    if (self.updatingCurrently && !force) {
       return;
     }
-    _updatingCurrently = YES;
+    self.updatingCurrently = YES;
   }
   
   [self runOnBackgroundQueue:^{
-    NSDictionary *properties = [self->_inMemoryStorage.storageDictionary copy];
+    NSDictionary *properties = [self.inMemoryStorage.storageDictionary copy];
     
     if (!properties || ![properties respondsToSelector:@selector(valueForKey:)]) {
       @synchronized (self) {
-        self->_updatingCurrently = NO;
+        self.updatingCurrently = NO;
       }
       return;
     }
     
     if (properties.count == 0) {
       @synchronized (self) {
-        self->_updatingCurrently = NO;
+        self.updatingCurrently = NO;
       }
       return;
     }
@@ -224,7 +224,7 @@ static NSString * const kBackgroundQueueName = @"qonversion.background.queue.nam
     block();
     return NO;
   } else {
-    [_backgroundQueue addOperationWithBlock:block];
+    [self.backgroundQueue addOperationWithBlock:block];
     return YES;
   }
 }
@@ -237,18 +237,18 @@ static NSString * const kBackgroundQueueName = @"qonversion.background.queue.nam
 }
 
 - (void)collectIntegrationsDataInBackground {
-  [_device adjustUserIDWithCompletion:^(NSString * _Nullable userId) {
+  [self.device adjustUserIDWithCompletion:^(NSString * _Nullable userId) {
     if (![QNUtils isEmptyString:userId]) {
       [self setUserProperty:@"_q_adjust_adid" value:userId];
     }
   }];
   
-  NSString *fbAnonID = _device.fbAnonID;
+  NSString *fbAnonID = self.device.fbAnonID;
   if (![QNUtils isEmptyString:fbAnonID]) {
     [self setUserProperty:@"_q_fb_anon_id" value:fbAnonID];
   }
   
-  NSString *afUserID = _device.afUserID;
+  NSString *afUserID = self.device.afUserID;
   if (![QNUtils isEmptyString:afUserID]) {
     [self setUserProperty:@"_q_appsflyer_user_id" value:afUserID];
   }
