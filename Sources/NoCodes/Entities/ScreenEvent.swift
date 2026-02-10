bd0d35ab20524f29ef0e4800a5fb5270b4293a6f
Daniil Fadeev <shameondev@gmail.com>
2026-02-10 18:33:22 +0300

    feat(analytics): add optional pageIndex to ScreenEvent for page_view events

--- Sources/NoCodes/Entities/ScreenEvent.swift
+++ Sources/NoCodes/Entities/ScreenEvent.swift
@@ -11,17 +11,30 @@ import Foundation
 struct ScreenEvent: Encodable {
   let type: ScreenEventType
   let screenUid: String
+  let pageIndex: Int?
   let happenedAt: Int
 
-  init(type: ScreenEventType, screenUid: String) {
+  init(type: ScreenEventType, screenUid: String, pageIndex: Int? = nil) {
     self.type = type
     self.screenUid = screenUid
+    self.pageIndex = pageIndex
     self.happenedAt = Int(Date().timeIntervalSince1970)
   }
 
   private enum CodingKeys: String, CodingKey {
     case type
     case screenUid = "screen_uid"
+    case pageIndex = "page_index"
     case happenedAt = "happened_at"
+  }
+
+  func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(type, forKey: .type)
+    try container.encode(screenUid, forKey: .screenUid)
+    if let pageIndex {
+      try container.encode(pageIndex, forKey: .pageIndex)
+    }
+    try container.encode(happenedAt, forKey: .happenedAt)
   }
 }
