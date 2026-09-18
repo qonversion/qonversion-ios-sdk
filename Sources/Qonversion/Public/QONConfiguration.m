99af7b132560e8fde5ea9fe7cdd81f6ca02300d2
GitHub Action <action@github.com>
2026-09-18 11:05:53 +0000

    Bump version to 6.17.1

--- Sources/Qonversion/Public/QONConfiguration.m
+++ Sources/Qonversion/Public/QONConfiguration.m
@@ -9,7 +9,7 @@
 #import "QONConfiguration.h"
 #import "QNAPIConstants.h"
 
-static NSString *const kSDKVersion = @"6.17.0";
+static NSString *const kSDKVersion = @"6.17.1";
 
 @interface QONConfiguration ()
 
